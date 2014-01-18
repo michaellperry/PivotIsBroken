0cecb8d1c18cb2a4c644280713166ea0f721f106
Michael L Perry <michael@qedcode.com>
2014-01-18 00:00:40 -0600

    Illustrate the problem with pivot contents not appearing.

--- PivotIsBroken/MainPage.xaml.cs
+++ PivotIsBroken/MainPage.xaml.cs
@@ -1,53 +1,47 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net;
-using System.Windows;
-using System.Windows.Controls;
+﻿using Microsoft.Phone.Controls;
+using System;
 using System.Windows.Navigation;
-using Microsoft.Phone.Controls;
-using Microsoft.Phone.Shell;
-using PivotIsBroken.Resources;
+using System.Windows.Threading;
 
 namespace PivotIsBroken
 {
     public partial class MainPage : PhoneApplicationPage
     {
-        // Constructor
         public MainPage()
         {
             InitializeComponent();
 
-            // Set the data context of the listbox control to the sample data
             DataContext = App.ViewModel;
-
-            // Sample code to localize the ApplicationBar
-            //BuildLocalizedApplicationBar();
         }
 
-        // Load data for the ViewModel Items
         protected override void OnNavigatedTo(NavigationEventArgs e)
         {
-            if (!App.ViewModel.IsDataLoaded)
+            App.ViewModel.Pages.Add(new ViewModels.PageViewModel
+                {
+                    Title = "Old"
+                }
+                .WithItem("One")
+                .WithItem("Two")
+                .WithItem("Three"));
+
+            DispatcherTimer timer = new DispatcherTimer();
+            timer.Interval = TimeSpan.FromSeconds(0.5);
+            timer.Tick += delegate
             {
-                App.ViewModel.LoadData();
-            }
+                App.ViewModel.Pages.RemoveAt(0);
+                App.ViewModel.Pages.Add(new ViewModels.PageViewModel
+                    {
+                        Title = "New"
+                    }
+                    .WithItem("Four")
+                    .WithItem("Five")
+                    .WithItem("Six"));
+                //App.ViewModel.Pages.Add(new ViewModels.PageViewModel());
+                //ApplicationPivot.SelectedIndex = 1;
+                //App.ViewModel.Pages.RemoveAt(1);
+                timer.Stop();
+            };
+            timer.Start();
         }
-
-        // Sample code for building a localized ApplicationBar
-        //private void BuildLocalizedApplicationBar()
-        //{
-        //    // Set the page's ApplicationBar to a new instance of ApplicationBar.
-        //    ApplicationBar = new ApplicationBar();
-
-        //    // Create a new button and set the text value to the localized string from AppResources.
-        //    ApplicationBarIconButton appBarButton = new ApplicationBarIconButton(new Uri("/Assets/AppBar/appbar.add.rest.png", UriKind.Relative));
-        //    appBarButton.Text = AppResources.AppBarButtonText;
-        //    ApplicationBar.Buttons.Add(appBarButton);
-
-        //    // Create a new menu item with the localized string from AppResources.
-        //    ApplicationBarMenuItem appBarMenuItem = new ApplicationBarMenuItem(AppResources.AppBarMenuItemText);
-        //    ApplicationBar.MenuItems.Add(appBarMenuItem);
-        //}
     }
 }
---
+++ PivotIsBroken/ViewModels/PageViewModel.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.ObjectModel;
+using System.ComponentModel;
+
+namespace PivotIsBroken.ViewModels
+{
+    public class PageViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private string _title;
+        private ObservableCollection<ItemViewModel> _items =
+            new ObservableCollection<ItemViewModel>();
+
+        public string Title
+        {
+            get { return _title; }
+            set
+            {
+                _title = value;
+                if (PropertyChanged != null)
+                    PropertyChanged(this, new PropertyChangedEventArgs("Title"));
+            }
+        }
+
+        public ObservableCollection<ItemViewModel> Items
+        {
+            get { return _items; }
+        }
+
+        public PageViewModel WithItem(string text)
+        {
+            _items.Add(new ItemViewModel { Text = text });
+            return this;
+        }
+    }
+}
